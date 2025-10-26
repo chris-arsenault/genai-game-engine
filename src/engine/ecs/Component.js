7aabba8878a4bb0abf3d50c28131c02bfdf8a4f0
Chris Arsenault <carsena2@gmail.com>
2025-10-25 22:36:39 -0700

    remove failed junk

--- src/engine/ecs/Component.js
+++
@@ -1,9 +0,0 @@
-/**
- * Base Component class
- * Components are data-only structures with no logic
- */
-export class Component {
-  constructor() {
-    // Override in subclasses with data fields
-  }
-}
